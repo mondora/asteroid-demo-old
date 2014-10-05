83287936438b104b8d4fa686b46456bf21dff661
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-10-05 22:36:10 +0200

    Refactored app.

--- browser/index.jsx
+++ browser/index.jsx
@@ -3,58 +3,68 @@
 (function () {
 
 
+	// TaskApp component
+	var TasksApp = React.createClass({
+		render: function () {
+			return (
+				<div className="col-xs-12 col-sm-6 col-sm-offset-3">
+					<h3>asteroid todo</h3>
+					<br />
+					<TaskForm />
+					<TasksList tasks={this.props.tasks} />
+					<br />
+				</div>
+			);
+		}
+	});
 
 	// TaskList component
 	var TasksList = React.createClass({
 		render: function () {
 			var tasksNodes = this.props.tasks.map(function (task) {
 				return (
-					<Task task={task} key={task._id} />
+					<TaskSummary task={task} key={task._id} />
 				);
 			});
-			var taskFormOrLoginButton = this.props.user ? <TaskForm user={this.props.user} /> : <LoginButton />;
 			return (
-				<div className="col-xs-12 col-sm-6 col-sm-offset-3">
-					<h3>asteroid todo</h3>
-					<br />
-					<ul className="list-group">
-						{tasksNodes}
-					</ul>
-					{taskFormOrLoginButton}
-				</div>
+				<ul className="list-group">
+					{tasksNodes}
+				</ul>
 			);
 		}
 	});
 
-	// Task component
-	var Task = React.createClass({
-		markAsDone: function () {
-			Ceres.call("markAsDone", this.props.task._id);
+	// TaskSummary component
+	var TaskSummary = React.createClass({
+		toggleStatus: function () {
+			Tasks.update(this.props.task._id, {
+				done: !this.props.task.done
+			});
 		},
 		deleteTask: function () {
 			Tasks.remove(this.props.task._id);
 		},
 		render: function () {
 			var done;
-			if (this.props.task.status === "done") {
-				done = <i className="fa fa-check pull-right"></i>;
-			} else if (this.props.task.userId) {
-				done = <button type="button" className="btn btn-xs btn-default pull-right" onClick={this.markAsDone}>Done</button>;
+			var description;
+			if (this.props.task.done) {
+				done = <i className="fa fa-check-square-o" onClick={this.toggleStatus}></i>;
+				description = <s>{this.props.task.description}</s>;
+			} else {
+				done = <i className="fa fa-square-o" onClick={this.toggleStatus}></i>;
+				description = this.props.task.description;
 			}
 			return (
 				<li className="list-group-item">
 					<div className="row">
-						<div className="col-xs-4">
-							{this.props.task.name}
-						</div>
-						<div className="col-xs-4">
-							{this.props.task.description}
-						</div>
-						<div className="col-xs-2">
+						<div className="col-xs-1">
 							{done}
 						</div>
-						<div className="col-xs-2">
-							<button type="button" className="btn btn-xs btn-default pull-right" onClick={this.deleteTask}>Delete</button>
+						<div className="col-xs-10">
+							{description}
+						</div>
+						<div className="col-xs-1">
+							<i className="fa fa-times" onClick={this.deleteTask}></i>
 						</div>
 					</div>
 				</li>
@@ -83,70 +93,63 @@
 			if (this.state.inputValue === "") {
 				return;
 			}
-			var task = {
-				name: this.props.user.profile.name,
-				userId: this.props.user._id,
+			Tasks.insert({
 				description: this.state.inputValue,
-				status: "todo"
-			};
-			Tasks.insert(task);
+				done: false
+			});
 			this.setState({
 				inputValue: ""
 			});
 		},
 		render: function () {
 			return (
-				<div className="input-group">
-					<input type="text" className="form-control" value={this.state.inputValue} onChange={this.userTypes} onKeyDown={this.addOnEnter} />
-					<span className="input-group-btn">
-						<button className="btn btn-default" type="button" onClick={this.addTask}>Add</button>
-					</span>
+				<div className="form-group">
+					<input className="form-control" placeholder="Add a task" autoFocus value={this.state.inputValue} onChange={this.userTypes} onKeyDown={this.addOnEnter} />
 				</div>
 			);
 		}
 	});
 
-	// LoginButton component
-	var LoginButton = React.createClass({
-		login: function () {
-			Ceres.loginWithGithub();
-		},
-		render: function () {
-			return (
-				<a className="btn btn-block btn-social btn-github" onClick={this.login}>
-					<i className="fa fa-github"></i> Sign in with Github to add a task
-				</a>
-			);
-		}
-	});
 
 
-
-	Ceres = new Asteroid("localhost:3000");
-
-	Ceres.subscribe("tasks");
-	Tasks = Ceres.getCollection("tasks");
-	Users = Ceres.getCollection("users");
-
-
-
-	var tasksRQ = Tasks.reactiveQuery({});
-	tasksRQ.on("change", render);
-	Ceres.on("login", render);
-	Ceres.on("logout", render);
-
-	function render () {
-		var user = Users.reactiveQuery({
-			_id: Ceres.userId
-		}).result[0];
-		var tasks = tasksRQ.result;
+	// Render function
+	function render (tasks) {
 		React.renderComponent(
-			<TasksList user={user} tasks={tasks} />,
-			document.getElementById("tasks-box")
+			<TasksApp tasks={tasks} />,
+			document.getElementById("app-container")
 		);
 	}
 
-	render();
+
+
+	// Tasks model
+	Tasks = {
+		insert: function (task) {
+			task._id = this._store.length;
+			this._store[task._id] = task;
+			this._onChange();
+		},
+		update: function (taskId, fields) {
+			var task = this._store[taskId];
+			Object.keys(fields).forEach(function (key) {
+				task[key] = fields[key];
+			});
+			this._onChange();
+		},
+		remove: function (taskId) {
+			delete this._store[taskId];
+			this._onChange();
+		},
+		_store: [],
+		_onChange: function () {
+			render(this._store);
+		}
+	};
+
+
+
+	// Bootstrap the app
+	render(Tasks._store);
 
 
 
